64784516dba08da8ad06248b71217ed8c8bbae11
YeoWool <gkaudrl542@daum.net>
2017-11-30 23:28:23 +0900

    Heap Sort with adjust , Space Time Complexity : O(1)

---
+++ Algorithm/src/homework3/Heap.java
@@ -0,0 +1,143 @@
+package homework3;
+
+public class Heap {
+	private final int SIZE = 200;
+	private int num;
+	private int[] element;
+	
+	public Heap(){
+		element = new int[SIZE];
+		num = 0;
+	}
+	public void getElement(){
+		for(int i = 0 ; i < element.length; i ++){
+			if(element[i] == 0) continue;
+			System.out.print(element[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public boolean isHeapEmpty(){
+		return num == 0 ;
+	}
+	public boolean isHeapFull(){
+		return num == SIZE-1;
+	}
+	
+	public void insert(int item){
+		int pos, par;
+		if(isHeapFull()) System.exit(1);
+		
+		if(num == 0){
+			element[num++] = item;
+		}else{
+			pos = num;
+			par = (pos-1)/2;
+			
+			while (item > element[par]){
+				element[pos] = element[par];
+				pos = par;
+				if(pos == 0) break;
+				par = (par-1)/2;
+			}
+			
+			element[pos] = item;
+			num++;
+		}
+	}
+	
+	public void heapSort(){
+		int i,j;
+		int temp;
+	
+		for(i = num-1; i>0 ; i--){
+			temp = element[0];
+			element[0] = element[i];
+			element[i] = temp;
+			adjust(0,i-1);
+		}
+	}
+	
+	public void adjust(int root,int last){
+		int pos = root;
+		int temp;
+		int l = pos*2 +1;
+		if(l > last) return;
+		int r = pos*2 +2;
+		if (r> last){
+			if(element[pos]<element[l]){
+				temp = element[l];
+				element[l] =element[pos];
+				element[pos] = temp;
+			}
+			return;
+		}
+		while(true){
+			if( element[l]> element[r] && element[l] > element[pos]){
+				temp = element[l];
+				element[l] = element[pos];
+				element[pos] = temp;
+				pos  = pos *2  +1 ;
+				
+				l = pos*2+1;
+				if( l > last) break;
+				r = pos*2+2;
+				if( r > last ){
+					if( element[pos]<element[l]){
+						temp = element[l];
+						element[l] = element[pos];
+						element[pos] = temp;
+					}
+					break;
+				}
+			}else if( element[l] < element[r] && element[r]> element[pos]){
+				temp = element[r];
+				element[r] = element[pos];
+				element[pos] = temp;
+				pos  = pos *2 +2;
+				l = pos*2 +1;
+				if( l > last) break;
+				r = pos*2 +2;
+				if( r > last ){
+					if( element[pos]<element[l]){
+						temp = element[l];
+						element[l] = element[pos];
+						element[pos] = temp;
+					}
+					break;
+				}
+			}
+				
+			
+		}
+		
+	}//end adjust
+	
+	
+	
+/*	public int delete(){
+		if(isHeapEmpty()) System.exit(1);
+		
+		int value = element[0];
+		int pos = 0 ;
+		num--;
+		if(isHeapEmpty()) return value;
+		
+		int last = num;
+		element[pos] = element[num];
+		while( pos <=last){
+			//one child
+			if( pos*2+2 > last){
+				
+			//two children
+			}else{
+				
+			}
+		}
+		
+		return value;
+		
+	}*/
+	
+	
+}
